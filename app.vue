42203d332fae61250f309162aadfe242b6cdfdce
César Augusto Costa <cesar.costa@nextios.com.br>
2024-11-29 17:16:49 -0300

    adiciona acao de alerta ao clicar no botao

--- app.vue
+++ app.vue
@@ -3,7 +3,17 @@
     <NuxtRouteAnnouncer />
     <div>
       <h1>HELLO WORLD</h1>
-      <button>TESTE</button>
+      <button @click="showAlert">TESTE</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    showAlert() {
+      alert("Botão clicado!"); // Mensagem do alerta
+    },
+  },
+};
+</script>
